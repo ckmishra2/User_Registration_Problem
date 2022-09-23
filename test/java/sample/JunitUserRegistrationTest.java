9a9107e4220ea19ae5b141d3e9d57b0a07809bb7
ckmishra2 <chandrakalatiwari2@gmail.com>
2022-09-23 20:59:17 +0530

    Add: For Valid Email

--- test/java/sample/JunitUserRegistrationTest.java
+++ test/java/sample/JunitUserRegistrationTest.java
@@ -25,7 +25,7 @@ public class JunitUserRegistrationTest {
 	}
 
 	@Test
-	public void givenLastNameIsProperReturnFalse() {
+	public void givenLastNameIsProperReturnTrue() {
 		boolean actualResult = junitUserRegistration.lastName("Mishra");
 		Assert.assertEquals(true, actualResult);
 
@@ -34,6 +34,14 @@ public class JunitUserRegistrationTest {
 	@Test
 	public void givenlastNameIsProperReturnFalse() {
 		boolean actualResult = junitUserRegistration.lastName("mishra");
+		Assert.assertEquals(false, actualResult);
+	}
+	public void givenemailIdIsProperReturnTrue() {
+		boolean actualResult = junitUserRegistration.emailId("abc.xyz@bl.co.in");
+		Assert.assertEquals(true, actualResult);
+	}
+	public void givenemailIdIsProperReturnFalse() {
+		boolean actualResult = junitUserRegistration.emailId("abc.xyz@bl.co.in.");
 		Assert.assertEquals(false, actualResult);
 	}
 }
